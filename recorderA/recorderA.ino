d4e9c803c51ad78da5e0215142216558d83e1d64
tosi <tosi31429180@gmail.com>
2026-05-27 14:12:17 +0900

    周波数の修正

--- recorderA/recorderA.ino
+++ recorderA/recorderA.ino
@@ -1,4 +1,5 @@
 float beat[] = {0.75,0.25,0.75,0.25,1.0,0.5,0.75,0.25,0.75,0.25,1.0,0.5,1.0,0.5,0.5,1.0,0.5,0.5,0.75,0.25,0.5,0.5,0.75,0.25,0.5,0.75,0.25,0.75,0.25,0.5,0.5,0.5,0.5,0.75,0.25,0.75,0.25,0.5,0.5,0.5,1.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.75,0.25,1.0};
+
 const int beatlength = sizeof(beat) / sizeof(beat[0]);
 int index = 0;
 
@@ -9,7 +10,7 @@ void setup() {
 
 void loop() {
   Serial.println("B");
-  delay(beat[index]*1000);   //  BPM
+  delay(beat[index]*1000);
   index++;
   if (index >= beatlength) {
     index = 0;
