3fed3a6c47c0bae9512c3bdb8819278a44cd7b62
tosi <tosi31429180@gmail.com>
2026-05-27 13:56:31 +0900

    音階の修正

--- recorderA/recorderA.ino
+++ recorderA/recorderA.ino
@@ -1,8 +1,18 @@
+float beat[] = {0.75,0.25,0.75,0.25,1.0,0.5,0.75,0.25,0.75,0.25,1.0,0.5,1.0,0.5,0.5,1.0,0.5,0.5,0.75,0.25,0.5,0.5,0.75,0.25,0.5,0.75,0.25,0.75,0.25,0.5,0.5,0.5,0.5,0.75,0.25,0.75,0.25,0.5,0.5,0.5,1.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.75,0.25,1.0};
+const int beatlength = sizeof(beat) / sizeof(beat[0]);
+int index = 0;
+
 void setup() {
   Serial.begin(9600);
+  int index = 0;
 }
 
 void loop() {
   Serial.println("B");
-  delay(500);   // 120 BPM
+  delay(beat[index]*1000);   //  BPM
+  index++;
+  if (index >= beatlength) {
+    index = 0;
+  }
+  
 }
